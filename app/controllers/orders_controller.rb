918c7882b99163ab0a18c3706e3f9c68d5815ddd
s-blais <stephen@s-blais.com>
2020-07-18 10:43:54 -0400

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,13 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+      #BEFORE using a serializer:
+      #render json: @post.to_json(only: [:title, :description, :id],
+      #                          include: [author: { only: [:name]}])
+       # AFTER USING OUR SERIALIZER
+    render json: @order, status: 200
+  end
+
 end
